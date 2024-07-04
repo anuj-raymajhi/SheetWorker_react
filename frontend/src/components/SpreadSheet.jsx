c0c480a4a4859f17d0652e26e4273483b16bfc0c
anuj_raymajhi <87628435+anuj-raymajhi@users.noreply.github.com>
2024-07-04 14:27:19 +0545

    working on cashflow worksheet
    added formula for profit after tax row

--- frontend/src/components/SpreadSheet.jsx
+++ frontend/src/components/SpreadSheet.jsx
@@ -2636,6 +2636,43 @@ function Spreadsheet() {
         }
     },[years])
 
+    // useEffect for profit after tax
+    useEffect(()=>{
+        if (years) {
+            var time_in_years = years.yearEnd - years.yearStart + 1;
+            var ColIndex = 1;
+            let update = [];
+            var temp;
+            var PLcol;
+            for (let i = 0; i < time_in_years; i++) {
+                temp = getOddNumberAtIndex(ColIndex)
+                PLcol = getSpreadsheetColumn(temp)
+                //17th row in PLSheet is net profit after tax
+                update.push( 
+                    {
+                        colVal:  ``,
+                        colSpan: 1,
+                        rowSpan: 1,
+                        index: ColIndex,
+                        isReadOnly: true,
+                        formula: `=PL!${PLcol}17`
+                    }
+                )
+                ColIndex += 1
+            }
+            setCfRowSheet(prevState => ({
+                ...prevState,
+                cash_from_operating_activities: {
+                    ...prevState.cash_from_operating_activities, 
+                    profit_after_tax: [
+                        ...prevState.cash_from_operating_activities.profit_after_tax,
+                        ...update
+                    ]
+                }
+            }))
+        }
+    },[years])
+
 
 
 
@@ -3974,6 +4011,7 @@ function Spreadsheet() {
                                                         rowSpan={value.rowSpan}
                                                         colSpan={value.colSpan}
                                                         isReadOnly={value.isReadOnly}
+                                                        formula={value.formula}
                                                     />
                                                 )
                                             }
