f9eb51ad6008f0edd182e30925f8233bc0d5c15e
Nishant Uprety <nishant.uprety@00-P3-D-NIC-62.nicasiabank.com>
2024-07-07 14:38:41 +0545

    Seven Controller completed

--- backend/controllers/BSheetController.js
+++ backend/controllers/BSheetController.js
@@ -1,59 +1,55 @@
 const {BSheetTable} = require('../models/BSheet')
 
-
-
-
 const yearsIndividualPostBSheet = async (req, res) => {
     const yearsData = req.body;
     console.log(yearsData);
 
     try {
-        const year = await PLTable.findOne({
+        const year = await BSheetTable.findOne({
             where: { Year_label: yearsData.Year_label }
         });
-
         if (year) {
             console.error('Year data already present');
             return res.status(409).send({ message: 'Year data already present' });
         }
 
 
-        // Ensure all attribute names match the model definition
-        const dataEntry = {
-            ShareCapital: yearsData.ShareCapital,
-            ReservesRetainedEarnings: yearsData.ReservesRetainedEarnings,
-            DirectorsLoanSubordinatedLoan: yearsData.DirectorsLoanSubordinatedLoan,
-            EffectiveNetworth: yearsData.EffectiveNetworth,
-            LongTermLoan: yearsData.LongTermLoan,
-            PrincipalLTLRepaidDuringYear: yearsData.PrincipalLTLRepaidDuringYear,
-            TotalLongTermLoan: yearsData.TotalLongTermLoan,
-            WorkingCapitalLoan: yearsData.WorkingCapitalLoan,
-            Creditors: yearsData.Creditors,
-            Payables: yearsData.Payables,
-            OtherCurrentLiabilitiesProvisions: yearsData.OtherCurrentLiabilitiesProvisions,
-            TotalCurrentLiabilities: yearsData.TotalCurrentLiabilities,
-            TotalCapitalLiabilities: yearsData.TotalCapitalLiabilities,
-            Land: yearsData.Land,
-            BuildingCivilStructureNetAfterDepreciation: yearsData.BuildingCivilStructureNetAfterDepreciation,
-            PlantMachineryNetAfterDepreciation: yearsData.PlantMachineryNetAfterDepreciation,
-            FurnitureFixturesNetAfterDepreciation: yearsData.FurnitureFixturesNetAfterDepreciation,
-            OfficeEquipmentNetAfterDepreciation: yearsData.OfficeEquipmentNetAfterDepreciation,
-            VehiclesNetAfterDepreciation: yearsData.VehiclesNetAfterDepreciation,
-            OtherFixedAssetsNetAfterDepreciation: yearsData.OtherFixedAssetsNetAfterDepreciation,
-            TotalFixedAssetsNetAfterDepreciation: yearsData.TotalFixedAssetsNetAfterDepreciation,
-            Investment: yearsData.Investment,
-            OtherIntangibleAssets: yearsData.OtherIntangibleAssets,
-            TotalLongTermInvestment: yearsData.TotalLongTermInvestment,
-            CashBank: yearsData.CashBank,
-            StoreStock: yearsData.StoreStock,
-            DebtorsReceivables: yearsData.DebtorsReceivables,
-            OtherCurrentAssets: yearsData.OtherCurrentAssets,
-            TotalCurrentAssets: yearsData.TotalCurrentAssets,
-            TotalAssets: yearsData.TotalAssets,
-            Diff: yearsData.Diff
-        };
+        // // Ensure all attribute names match the model definition
+        // const dataEntry = {
+        //     ShareCapital: yearsData.ShareCapital,
+        //     ReservesRetainedEarnings: yearsData.ReservesRetainedEarnings,
+        //     DirectorsLoanSubordinatedLoan: yearsData.DirectorsLoanSubordinatedLoan,
+        //     EffectiveNetworth: yearsData.EffectiveNetworth,
+        //     LongTermLoan: yearsData.LongTermLoan,
+        //     PrincipalLTLRepaidDuringYear: yearsData.PrincipalLTLRepaidDuringYear,
+        //     TotalLongTermLoan: yearsData.TotalLongTermLoan,
+        //     WorkingCapitalLoan: yearsData.WorkingCapitalLoan,
+        //     Creditors: yearsData.Creditors,
+        //     Payables: yearsData.Payables,
+        //     OtherCurrentLiabilitiesProvisions: yearsData.OtherCurrentLiabilitiesProvisions,
+        //     TotalCurrentLiabilities: yearsData.TotalCurrentLiabilities,
+        //     TotalCapitalLiabilities: yearsData.TotalCapitalLiabilities,
+        //     Land: yearsData.Land,
+        //     BuildingCivilStructureNetAfterDepreciation: yearsData.BuildingCivilStructureNetAfterDepreciation,
+        //     PlantMachineryNetAfterDepreciation: yearsData.PlantMachineryNetAfterDepreciation,
+        //     FurnitureFixturesNetAfterDepreciation: yearsData.FurnitureFixturesNetAfterDepreciation,
+        //     OfficeEquipmentNetAfterDepreciation: yearsData.OfficeEquipmentNetAfterDepreciation,
+        //     VehiclesNetAfterDepreciation: yearsData.VehiclesNetAfterDepreciation,
+        //     OtherFixedAssetsNetAfterDepreciation: yearsData.OtherFixedAssetsNetAfterDepreciation,
+        //     TotalFixedAssetsNetAfterDepreciation: yearsData.TotalFixedAssetsNetAfterDepreciation,
+        //     Investment: yearsData.Investment,
+        //     OtherIntangibleAssets: yearsData.OtherIntangibleAssets,
+        //     TotalLongTermInvestment: yearsData.TotalLongTermInvestment,
+        //     CashBank: yearsData.CashBank,
+        //     StoreStock: yearsData.StoreStock,
+        //     DebtorsReceivables: yearsData.DebtorsReceivables,
+        //     OtherCurrentAssets: yearsData.OtherCurrentAssets,
+        //     TotalCurrentAssets: yearsData.TotalCurrentAssets,
+        //     TotalAssets: yearsData.TotalAssets,
+        //     Diff: yearsData.Diff
+        // };
 
-        await BSheetTable.create(dataEntry)
+        await BSheetTable.create(yearsData)
             .then((data) => {
                 console.log('Data entry successful');
                 return res.status(200).send({ message: 'Row added to table' });
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,12 +4,25 @@ const router = express.Router();
 const { yearsData, yearsIndividual,yearsIndividualPost } = require('../controllers/PLController');
 const {yearsIndividualPostBSheet} = require('../controllers/BSheetController')
 const {yearsIndividualPostCashFlow} = require('../controllers/CashFlowController')
+const {yearsIndividualPostRiskGrading} = require('../controllers/RiskGradingController')
+
+const {yearsIndividualPostKeyRatios} = require('../controllers/KeyRatiosController')
+const {yearsIndividualPostKFA} = require('../controllers/KFAController')
+const {yearsIndividualPostSummaryCashFlow} = require('../controllers/SummaryCashFlowController')
 
 router.get('/years', yearsData);
 router.get('/year/:yearId', yearsIndividual);
-router.post('/yearsEntry',yearsIndividualPost)
 
+router.post('/yearsEntry',yearsIndividualPost)
 router.post('/yearsEntryBSheet',yearsIndividualPostBSheet)
 router.post('/yearsEntryCashFlow',yearsIndividualPostCashFlow)
+router.post('/yearsEntryRiskGrading',yearsIndividualPostRiskGrading)
+router.post('/yearsEntryKeyRatios',yearsIndividualPostKeyRatios)
+router.post('/yearsEntryKFA',yearsIndividualPostKFA)
+router.post('/yearsEntrySummaryCashFlow',yearsIndividualPostSummaryCashFlow)
+
+router.post('/yearsEntryKFA',yearsIndividualPostKFA)
+
+
 
 module.exports = router;
