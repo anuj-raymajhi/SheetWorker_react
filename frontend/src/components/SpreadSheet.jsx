403f47f474e209df940657e3a4138ad952cf4038
anuj_raymajhi <87628435+anuj-raymajhi@users.noreply.github.com>
2024-07-08 12:23:09 +0545

    working on summary worksheet
    key financials assessment: term loan to net fixed asset row formula intergration

--- frontend/src/components/SpreadSheet.jsx
+++ frontend/src/components/SpreadSheet.jsx
@@ -4892,6 +4892,41 @@ function Spreadsheet() {
         }
     },[years])
 
+    // useEffect for term loan to net fixed asset row
+    useEffect(()=>{
+        if (years) {
+            var time_in_years = years.yearEnd - years.yearStart + 1;
+            var ColIndex = 1;
+            let update = [];
+            var currCol;
+            for (let i = 0; i < time_in_years; i++) {
+                currCol = getSpreadsheetColumn(ColIndex)
+
+                update.push( 
+                    {
+                        colVal:  ``,
+                        colSpan: 1,
+                        rowSpan: 1,
+                        index: ColIndex,
+                        isReadOnly: true,
+                        formula: `=IFERROR(${currCol}12/${currCol}11, "NA")`
+                    }
+                )
+                ColIndex += 1
+            }
+            setSummRowSheet(prevState => ({
+                ...prevState,
+                key_financial_assesments: {
+                    ...prevState.key_financial_assesments, 
+                    term_loan_to_net_fixed_assets: [
+                        ...prevState.key_financial_assesments.term_loan_to_net_fixed_assets,
+                        ...update
+                    ]
+                }
+            }))
+        }
+    },[years])
+
     // useEffect for Risk Grading section
     // useEffect for risk grading header row
     useEffect(()=>{
@@ -7416,6 +7451,7 @@ function Spreadsheet() {
                                                         rowSpan={value.rowSpan}
                                                         colSpan={value.colSpan}
                                                         isReadOnly={value.isReadOnly}
+                                                        formula={value.formula}
                                                     />
                                                 )
                                             }
