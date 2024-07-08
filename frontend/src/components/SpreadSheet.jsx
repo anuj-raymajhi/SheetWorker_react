17562038bf1bff1af3e9aff0f817227c53553b2a
anuj_raymajhi <87628435+anuj-raymajhi@users.noreply.github.com>
2024-07-08 15:29:59 +0545

    working on summary worksheet
    key ratios: debt service coverage ratio row permission set to write.

--- frontend/src/components/SpreadSheet.jsx
+++ frontend/src/components/SpreadSheet.jsx
@@ -5482,6 +5482,44 @@ function Spreadsheet() {
         }
     },[years])
 
+    // useEffect of debt service coverage ratio row
+    useEffect(()=>{
+        if (years) {
+            var time_in_years = years.yearEnd - years.yearStart + 1;
+            var ColIndex = 1;
+            let update = [];
+            var currCol;
+            var PLcol;
+            for (let i = 0; i < time_in_years; i++) {
+                PLcol = getOddNumberAtIndex(ColIndex)
+                PLcol = getSpreadsheetColumn(PLcol)
+                currCol = getSpreadsheetColumn(ColIndex)
+
+                update.push( 
+                    {
+                        colVal:  ``,
+                        colSpan: 1,
+                        rowSpan: 1,
+                        index: ColIndex,
+                        isReadOnly: true,
+                        formula: `=IFERROR((PL!${PLcol}17+PL!${PLcol}15+PL!${PLcol}12)/(PL!${PLcol}12+'BSheet & Ratios'!${currCol}9), "NA")`
+                    }
+                )
+                ColIndex += 1
+            }
+            setSummRowSheet(prevState => ({
+                ...prevState,
+                key_ratios: {
+                    ...prevState.key_ratios, 
+                    debt_service_coverage_ratio: [
+                        ...prevState.key_ratios.debt_service_coverage_ratio,
+                        ...update
+                    ]
+                }
+            }))
+        }
+    },[years])
+
     // useEffect for cash flow section
     // useEffect for cash flow header
     useEffect(()=>{
@@ -8199,6 +8237,7 @@ function Spreadsheet() {
                                                         rowSpan={value.rowSpan}
                                                         colSpan={value.colSpan}
                                                         isReadOnly={value.isReadOnly}
+                                                        formula={value.formula}
                                                     />
                                                 )
                                             }
