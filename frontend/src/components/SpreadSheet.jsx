2191846db9a48b18b9eacb1a047c19f5e6a9e42f
anuj_raymajhi <87628435+anuj-raymajhi@users.noreply.github.com>
2024-07-08 15:33:16 +0545

    working on summary worksheet
    key ratios: interest coverage ratio row formula integrated.

--- frontend/src/components/SpreadSheet.jsx
+++ frontend/src/components/SpreadSheet.jsx
@@ -5520,6 +5520,42 @@ function Spreadsheet() {
         }
     },[years])
 
+    // useEffect for interest coverage ratio row
+    useEffect(()=>{
+        if (years) {
+            var time_in_years = years.yearEnd - years.yearStart + 1;
+            var ColIndex = 1;
+            let update = [];
+            var PLcol;
+            for (let i = 0; i < time_in_years; i++) {
+                PLcol = getOddNumberAtIndex(ColIndex)
+                PLcol = getSpreadsheetColumn(PLcol)
+
+                update.push( 
+                    {
+                        colVal:  ``,
+                        colSpan: 1,
+                        rowSpan: 1,
+                        index: ColIndex,
+                        isReadOnly: true,
+                        formula: `=IFERROR(PL!${PLcol}22/PL!${PLcol}12, "NA")`
+                    }
+                )
+                ColIndex += 1
+            }
+            setSummRowSheet(prevState => ({
+                ...prevState,
+                key_ratios: {
+                    ...prevState.key_ratios, 
+                    interest_coverage_ratio: [
+                        ...prevState.key_ratios.interest_coverage_ratio,
+                        ...update
+                    ]
+                }
+            }))
+        }
+    },[years])
+
     // useEffect for cash flow section
     // useEffect for cash flow header
     useEffect(()=>{
@@ -8259,6 +8295,7 @@ function Spreadsheet() {
                                                         rowSpan={value.rowSpan}
                                                         colSpan={value.colSpan}
                                                         isReadOnly={value.isReadOnly}
+                                                        formula={value.formula}
                                                     />
                                                 )
                                             }
