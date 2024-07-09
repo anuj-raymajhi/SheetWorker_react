63bf5bc71f1058fc4672cafca95494b719823515
Nishant Uprety <nishant.uprety@00-P3-D-NIC-62.nicasiabank.com>
2024-07-09 10:25:06 +0545

    PL Table controller finalized

--- backend/controllers/PLController.js
+++ backend/controllers/PLController.js
@@ -88,45 +88,6 @@ const yearsIndividualPost = async (req, res) => {
             console.error('Year data already present');
             return res.status(409).send({ message: 'Year data already present' });
         }
-
-        // Ensure all attribute names match the model definition
-        // const dataEntry = {
-        //     Year_label: yearsData.Year_label,
-        //     audited_projected: yearsData.audited_projected,
-        //     capacity_utilization: yearsData.capacity_utilization,
-        //     amount_total_revenue_sales: yearsData.amount_total_revenue_sales,
-        //     amount_cost_of_goods_sold: yearsData.amount_cost_of_goods_sold,
-        //     amount_other_direct_expenses: yearsData.amount_other_direct_expenses,
-        //     amount_gross_profit: yearsData.amount_gross_profit,
-        //     amount_other_income: yearsData.amount_other_income,
-        //     amount_gain_loss_on_sale_of_assest: yearsData.amount_gain_loss_on_sale_of_assest,
-        //     amount_office_administrative_overhead: yearsData.amount_office_administrative_overhead,
-        //     amount_financial_expenses_interest_exp: yearsData.amount_financial_expenses_interest_exp,
-        //     amount_selling_and_distribution_expenses: yearsData.amount_selling_and_distribution_expenses,
-        //     amount_profit_before_depreciation_and_tax: yearsData.amount_profit_before_depreciation_and_tax,
-        //     amount_depreciation_expenses: yearsData.amount_depreciation_expenses,
-        //     amount_income_tax: yearsData.amount_income_tax,
-        //     amount_net_profit_after_tax: yearsData.amount_net_profit_after_tax,
-        //     amount_profit_upto_last_year: yearsData.amount_profit_upto_last_year,
-        //     amount_withdrawal_dividend_drawing: yearsData.amount_withdrawal_dividend_drawing,
-        //     amount_provision: yearsData.amount_provision,
-        //     amount_profit_transferred_to_balanced_sheet: yearsData.amount_profit_transferred_to_balanced_sheet,
-        //     amount_earning_before_interest_and_tax: yearsData.amount_earning_before_interest_and_tax,
-        //     percent_total_revenue_sales: yearsData.percent_total_revenue_sales,
-        //     percent_cost_of_goods_sold: yearsData.percent_cost_of_goods_sold,
-        //     percent_other_direct_expenses: yearsData.percent_other_direct_expenses,
-        //     percent_gross_profit: yearsData.percent_gross_profit,
-        //     percent_other_income: yearsData.percent_other_income,
-        //     percent_gain_loss_on_sale_of_assest: yearsData.percent_gain_loss_on_sale_of_assest,
-        //     percent_office_administrative_overhead: yearsData.percent_office_administrative_overhead,
-        //     percent_financial_expenses_interest_exp: yearsData.percent_financial_expenses_interest_exp,
-        //     percent_selling_and_distribution_expenses: yearsData.percent_selling_and_distribution_expenses,
-        //     percent_profit_before_depreciation_and_tax: yearsData.percent_profit_before_depreciation_and_tax,
-        //     percent_depreciation_expenses: yearsData.percent_depreciation_expenses,
-        //     percent_income_tax: yearsData.percent_income_tax,
-        //     percent_net_profit_after_tax: yearsData.percent_net_profit_after_tax
-        // };
-
         await PLTable.create(yearsData)
             .then((data) => {
                 console.log('Data entry successful');
