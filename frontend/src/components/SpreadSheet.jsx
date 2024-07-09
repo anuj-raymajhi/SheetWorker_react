56b3d3b3fb25dee290ff32cf1375d2ffba0677de
anuj_raymajhi <87628435+anuj-raymajhi@users.noreply.github.com>
2024-07-09 10:35:09 +0545

    working on summary worksheet
    key ratios: sales to WCL ratio row formula integrated.

--- frontend/src/components/SpreadSheet.jsx
+++ frontend/src/components/SpreadSheet.jsx
@@ -5661,6 +5661,44 @@ function Spreadsheet() {
         }
     },[years])
 
+    // useEffect for sales to WCL ratio row
+    useEffect(()=>{
+        if (years) {
+            var time_in_years = years.yearEnd - years.yearStart + 1;
+            var ColIndex = 1;
+            let update = [];
+            var currCol;
+            var PLcol;
+            for (let i = 0; i < time_in_years; i++) {
+                PLcol = getOddNumberAtIndex(ColIndex)
+                PLcol = getSpreadsheetColumn(PLcol)
+                currCol = getSpreadsheetColumn(ColIndex)
+
+                update.push( 
+                    {
+                        colVal:  ``,
+                        colSpan: 1,
+                        rowSpan: 1,
+                        index: ColIndex,
+                        isReadOnly: true,
+                        formula: `=IFERROR((+PL!${PLcol}8)/'BSheet & Ratios'!${currCol}14, "NA")`
+                    }
+                )
+                ColIndex += 1
+            }
+            setSummRowSheet(prevState => ({
+                ...prevState,
+                key_ratios: {
+                    ...prevState.key_ratios, 
+                    sales_to_WCL_ratio: [
+                        ...prevState.key_ratios.sales_to_WCL_ratio,
+                        ...update
+                    ]
+                }
+            }))
+        }
+    },[years])
+
     // useEffect for cash flow section
     // useEffect for cash flow header
     useEffect(()=>{
@@ -8488,6 +8526,7 @@ function Spreadsheet() {
                                                         rowSpan={value.rowSpan}
                                                         colSpan={value.colSpan}
                                                         isReadOnly={value.isReadOnly}
+                                                        formula={value.formula}
                                                     />
                                                 )
                                             }
